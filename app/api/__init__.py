1659a1f21e5c6309e60691cc1a3eb4bd710c503c
Diego Chan <d.chan@masmedan.com>
2026-08-27 11:39:30 -0500

    Refactor health check response logging and update healthcheck timeout in deployment configuration

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import APIRouter, Response
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
@@ -5,6 +7,8 @@ from sqlalchemy.exc import SQLAlchemyError
 from app.api import v1
 from app.dependencies import RequiresLocalDB, RequiresRemoteDB
 from app.schemas.internal import AppMessage
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix='/api')
 
@@ -25,14 +29,16 @@ async def health(response: Response, lcl: RequiresLocalDB, rmt: RequiresRemoteDB
         remote_ok = False
 
     if not local_ok:
-        message = AppMessage.warning('Base de datos local inaccesible')
+        msg = AppMessage.warning('Base de datos local inaccesible')
     elif not remote_ok:
-        message = AppMessage.warning('Base de datos remota inaccesible')
+        msg = AppMessage.warning('Base de datos remota inaccesible')
     else:
-        message = AppMessage.success('Todos los sistemas operativos')
+        msg = AppMessage.success('Todos los sistemas operativos')
 
     response.status_code = 200 if local_ok and remote_ok else 503
 
-    return { 'status': response.status_code, **message.dict() }
+    logger.info(msg.message)
+
+    return { 'status': response.status_code, **msg.dict() }
 
 router.include_router(v1.router)
